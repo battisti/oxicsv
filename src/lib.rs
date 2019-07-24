d0f78938d37f65ad5b7ed674832e24e0870c32a7
Alexander Battisti <alexander.battisti@eon.com>
2019-07-24 17:32:39 +0200

    reading csv

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-use std::path::PathBuf;
+use csv::StringRecord;
+use std::error::Error;
+use std::fs::File;
+use std::path::{Path, PathBuf};
 
 fn get_path_to_csv() -> PathBuf {
     let mut local_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
@@ -7,19 +10,33 @@ fn get_path_to_csv() -> PathBuf {
     local_path
 }
 
+fn read_csv_from_path(path: &Path) -> Result<Vec<StringRecord>, Box<dyn Error>> {
+    let mut reader = csv::Reader::from_path(path)?;
+
+    Ok(reader.records().filter_map(Result::ok).collect())
+}
+
 #[cfg(test)]
 mod test {
 
-    use super::get_path_to_csv;
-    use std::fs::{metadata, Metadata};
-    use std::io::Error;
+    use super::*;
 
     #[test]
-    fn get_path_to_csv_should_locate_file() {
+    fn should_locate_csv_file() {
+        use std::fs::{metadata, Metadata};
+
         let path = get_path_to_csv();
         let is_file = metadata(&path).as_ref().map(Metadata::is_file).unwrap();
 
         assert!(is_file);
+    }
+
+    #[test]
+    fn should_read_all_records() {
+        let path = get_path_to_csv();
+        let records = read_csv_from_path(&path).unwrap();
+
+        assert_eq!(records.len(), 550);
     }
 
 }
